bca63799125d493d63ae4d0d44331e1cc27ab21d
aglahir <aglahirjf@gmail.com>
2020-04-04 00:26:33 -0600

    V1.0 Added repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab 6 Web
+
+>This repository contains the 6th laboratory for my web development class.
+
+This lab shows connection of YouTube API into a sample website with html, javascript and css
---
+++ js/index.js
@@ -0,0 +1,83 @@
+// A01364026 Aglahir Jimenez Florez
+
+const API_KEY = "AIzaSyD-xAyMePzHLDye8BP6CRnmRy3VBhIO64o";
+const maxResults = 10;
+
+function displayResults(jsonIn, term)
+{
+    let results = document.querySelector( '.results' );
+    results.innerHTML = "";
+
+    for( let i = 0; i < jsonIn.items.length; i ++){
+        results.innerHTML += `
+            <a href="https://www.youtube.com/watch?v=${jsonIn.items[i].id.videoId}" target="_blank">
+              <div class="videoItem">
+                  <img src="${jsonIn.items[i].snippet.thumbnails.medium.url}" />
+                  <h3>
+                      ${jsonIn.items[i].snippet.title}
+                  </h3>
+              </div>
+            </a>
+        `;
+    }
+  
+    let pageButtons = '<div class="pageButtons">';
+  
+    if(jsonIn.prevPageToken != null){
+      pageButtons += `
+          <button class="pageButton" onclick="fetchVideos('${term}', '${jsonIn.prevPageToken}')">Prev</button>
+      `;
+    }
+    
+    pageButtons += `
+        <button class="pageButton" onclick="fetchVideos('${term}', '${jsonIn.nextPageToken}')">Next</button>
+      </div>
+    `;
+  
+  results.innerHTML += pageButtons;
+}
+
+function fetchVideos( term, pageToken )
+{
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&maxResults=${maxResults}&type=video&order=relevance&q=${encodeURIComponent(term.trim())}`;
+    if(pageToken != null) url += `&pageToken=${pageToken}`;
+  
+    let settings = {
+        method : 'GET'
+    };
+    console.log(url);
+    fetch( url, settings )
+        .then( response => {
+            if(response.ok)
+            {
+                return response.json();
+            }
+
+            throw new Error( response.statusText );
+        })
+        .then( responseJSON => {
+            displayResults(responseJSON, term);
+        })
+        .catch( err => {
+            console.log( err );
+        });
+}
+
+function watchForm()
+{
+    let submitBtn = document.querySelector( '.submitButton' );
+
+    submitBtn.addEventListener( 'click', (event) => {
+        event.preventDefault();
+        let searchTerm = document.querySelector( '#searchTerm' ).value;
+
+        fetchVideos( searchTerm, null);
+    });
+}
+
+function init()
+{
+    watchForm();
+}
+
+init();
